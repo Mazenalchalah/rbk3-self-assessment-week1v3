c7f1523e86dbbc281a4b058eca4497389d7d4506
mazenalchalah <mazenalchalah@gmail.com>
2017-08-30 23:52:38 +0300

    refactor

--- ds-hash-table/ds-hash-table.js
+++ ds-hash-table/ds-hash-table.js
@@ -5,16 +5,42 @@ var makeHashTable = function() {
     return {
       _storage: [],
       retrieve: function(key) {
+      	var bucketIndex = hashFn(key, max);
+      var bucket = this._storage[bucketIndex];
+
+      for (var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+        if (tuple[0] === key) {
+          return tuple[1];
+        };
+      };
+      return null;
         return this._storage[hashFn(key, max)];
       },
 
       insert: function(key, value) {
 
         //your code is here
-        
-        this._storage[hashFn(key, max)] = value;
+      var bucketIndex = hashFn(key, max)
+       
+      var bucket = this._storage[bucketIndex] || []; 
+      this._storage[bucketIndex] = bucket;
+      for (var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+        if (tuple[0] === key) {
+          tuple[1] = value;
+          return;
+        };
+      };
+
+
+
+      bucket.push([key, value]);
     }
-  }
+
+  };
+    
+  
 };
 
 // This is a "hashing function". You don't need to worry about it, just use it to turn any key into a pseudo-random key
--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,29 +1,19 @@
 var Stack = function() {
-    this._storage ={};
-    this._counter=0;
-    this._add=add;
-    this._remove=remove;
+   this._storage =[];
+   
+   
+
     this.add = function(value){
 
       // write me
-      this._counter++;//run the counter 
-      this._storage[this._counter]=value;//insert value
-      return this._storage[this._counter];//return the insert value
+      
+      this._storage.push(value);//insert value
+      
     };
     this.remove = function() {
       // write me
-      if(this._counter>0)
-      {
-      	var result =this._storage[this._counter];
-      	    delete this._storage[this._counter];//delete the storage if counter more than 0 exixst
-      	       this._counter--;//decrease the counter
-      	       return result;//return the result that i deleted
-      	      
+    
+      	var result =this._storage.pop()
+      	    return result;
 
-      }
-         
-         
-     
-      
-    };
-  };
+  };}
